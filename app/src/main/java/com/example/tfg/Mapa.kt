5a2255076b64ed1e94da0699c3d2f94d3e548cd4
luisacho5 <luis.sanchez3@hotmail.es>
2022-05-31 14:16:47 +0200

    feat: move to current location

--- app/src/main/java/com/example/tfg/Mapa.kt
+++ app/src/main/java/com/example/tfg/Mapa.kt
@@ -14,6 +14,7 @@ import android.provider.Settings
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -26,128 +27,143 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class Mapa : AppCompatActivity(), OnMapReadyCallback {
+class Mapa : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapaBinding
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
 
+    companion object{
+        const val LOCATION_REQUEST_CODE = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapaBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
         val email: String? = intent.getStringExtra("email")
-        val name:String?=intent.getStringExtra("name")
-
+        val name: String? = intent.getStringExtra("name")
+        createFragment()
         val navigation: BottomNavigationView = findViewById(R.id.menu)
-        navigation.setOnItemSelectedListener {item ->
-            when(item.itemId){
-                R.id.profile->{
-                    val homeIntent = Intent(this,Profile::class.java)
-                    homeIntent.putExtra("email",email)
-                    homeIntent.putExtra("name",name)
+        navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.profile -> {
+                    val homeIntent = Intent(this, Profile::class.java)
+                    homeIntent.putExtra("email", email)
+                    homeIntent.putExtra("name", name)
                     startActivity(homeIntent)
                     true
                 }
-                R.id.chat->{
-                    val homeIntent = Intent(this,Chat::class.java)
-                    homeIntent.putExtra("email",email)
-                    homeIntent.putExtra("name",name)
+                R.id.chat -> {
+                    val homeIntent = Intent(this, Chat::class.java)
+                    homeIntent.putExtra("email", email)
+                    homeIntent.putExtra("name", name)
                     startActivity(homeIntent)
                     true
                 }
-                R.id.comunidad->{
-                    val homeIntent = Intent(this,Community::class.java)
-                    homeIntent.putExtra("email",email)
-                    homeIntent.putExtra("name",name)
+                R.id.comunidad -> {
+                    val homeIntent = Intent(this, Community::class.java)
+                    homeIntent.putExtra("email", email)
+                    homeIntent.putExtra("name", name)
                     startActivity(homeIntent)
                     true
                 }
-                else->{
-                    val homeIntent = Intent(this,Mapa::class.java)
-                    homeIntent.putExtra("email",email)
-                    homeIntent.putExtra("name",name)
+                else -> {
+                    val homeIntent = Intent(this, Mapa::class.java)
+                    homeIntent.putExtra("email", email)
+                    homeIntent.putExtra("name", name)
                     startActivity(homeIntent)
                     true
                 }
             }
         }
-        fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
+
 
     }
+
+
+
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        // Add a marker in Madrid
-        val aux=getCurrentLocation()
-        val sydney = LatLng(aux.latitud, aux.longitud)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Location"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        createMarker()
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        enableLocation()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
 
-        private data class Aux(val latitud:Double,val longitud:Double)
+    private fun createMarker() {
+        val coordinates = LatLng(28.043839, -16.539)
+        val marker = MarkerOptions().position(coordinates).title("La playita")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLng(coordinates),
+            4000,
+            null
+        )
 
-        @SuppressLint("MissingPermission")
-        private fun getCurrentLocation():Aux{
-            var latitud:Double=0.0
-            var longitud:Double=0.0
-            if(checkPermissions())
-            {
-                if(isLocationEnabled()){
-                    fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){task ->
-                        val locationaux: Location? =task.result
-                        if (locationaux==null){
-                            Toast.makeText(applicationContext,"Null received",Toast.LENGTH_SHORT).show()
-                        }else {
-                            latitud = locationaux.latitude
-                            longitud = locationaux.longitude
-                        }
-                    }
-                }else{
-                    Toast.makeText(applicationContext,"Turn on location",Toast.LENGTH_SHORT).show()
-                    val intent =Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                    startActivity(intent)
-                }
+    }
+
+    private fun isLocationPermissionGranted() =
+        ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation(){
+        if(!::map.isInitialized) return
+        if(isLocationPermissionGranted()){
+            map.isMyLocationEnabled= true
             }else{
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
-            }
-            return Aux(latitud,longitud)
+            askPermissions()
         }
-        private fun isLocationEnabled():Boolean{
-            val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-        }
+    }
 
-        companion object{
-            private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=100
+    private fun askPermissions(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this, "Ve a ajustes y acepta permisos ",Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_REQUEST_CODE)
         }
-        private fun checkPermissions():Boolean{
-            return (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
-        }
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION){
-            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(applicationContext,"Granted",Toast.LENGTH_SHORT).show()
-                getCurrentLocation()
+        when(requestCode){
+            LOCATION_REQUEST_CODE -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                map.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(this, "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
             }
-            else{
-                Toast.makeText(applicationContext,"DENIED",Toast.LENGTH_SHORT).show()
-            }
+            else -> {}
         }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (!::map.isInitialized) return
+        if(!isLocationPermissionGranted()){
+            map.isMyLocationEnabled = false
+            Toast.makeText(this, "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        return false
+    }
+
+    override fun onMyLocationClick(p0: Location) {
     }
 
 }
